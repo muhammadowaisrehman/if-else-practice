ae0e62cc3620978d084be94fc4b43504a7bf0785
muhammadowaisrehman <m.owais.r.k@gmail.com>
2023-06-18 16:01:08 +0500

    practice

---
+++ main.dart
@@ -0,0 +1,13 @@
+main() {
+  num year = 1995;
+
+  if (year % 4 != 0) {
+    print('$year is a common year.');
+  } else if (year % 100 != 0) {
+    print('$year is a leap year.');
+  } else if (year % 400 != 0) {
+    print('$year is a common year.');
+  } else {
+    print('$year is a leap year.');
+  }
+}
